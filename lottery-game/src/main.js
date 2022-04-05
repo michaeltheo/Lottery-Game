8abac7fca8e81049b62360d73d8a7bf7b8f55736
michaeltheo <michaeltheochar@gmail.com>
2022-04-05 13:58:05 +0300

    ínitialize Firebase

--- lottery-game/src/main.js
+++ lottery-game/src/main.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app'
+
+
+
+var firebaseConfig={
+  apiKey: "AIzaSyAH71C0NybNd0YTTa8PUGSJJ0KVqQmkwdw",
+
+    authDomain: "lottery-game-46e16.firebaseapp.com",
+
+    projectId: "lottery-game-46e16",
+
+    storageBucket: "lottery-game-46e16.appspot.com",
+
+    messagingSenderId: "983292448283",
+
+    appId: "1:983292448283:web:67630bf19be7d8239072be",
+
+    measurementId: "G-HNF68NRWY0"
+
+
+}
+//Initialize firebase
+firebase.initializeApp(firebaseConfig)
+
 
 Vue.config.productionTip = false
 
